66c42fd9830f72709f537c6fa91932ce08562689
linporu <qazwsx123456guy@gmail.com>
2024-11-15 18:11:14 +0800

    chore: set up Django debug toolbar

--- Project 2 commerce/commerce/auctions/urls.py
+++ Project 2 commerce/commerce/auctions/urls.py
@@ -1,5 +1,5 @@
-from django.urls import path
-
+from django.urls import include, path
+from django.conf import settings
 from . import views
 
 urlpatterns = [
@@ -11,3 +11,8 @@ urlpatterns = [
     path("active-listings", views.active_listings, name="active_listings"),
     path("listing/<int:listing_id>", views.listing_page, name="listing_page")
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path('__debug__/', include('debug_toolbar.urls')),
+    ]
--- Project 2 commerce/commerce/commerce/settings.py
+++ Project 2 commerce/commerce/commerce/settings.py
@@ -38,9 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar'
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -124,3 +126,6 @@ STATIC_URL = '/static/'
 # Other settings
 LOGIN_URL = 'login'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
