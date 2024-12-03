bad9e63604a4d13f3b9dffdef9c675db77d08dc3
linporu <qazwsx123456guy@gmail.com>
2024-12-03 18:39:35 +0800

    feat: add Post, Comment, Like, and Following models
    - Introduced new models for Post, Comment, Like, and Following to enhance the social networking features of the application.
    - Each Post can have multiple likes and comments, with properties to count them.
    - Implemented unique constraints for Following and Like models to prevent duplicate entries.
    - Added timestamps for creation and updates to track user interactions effectively.

--- Project 4 network/network/network/models.py
+++ Project 4 network/network/network/models.py
@@ -4,3 +4,80 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Post(models.Model):
+    content = models.TextField()
+    created_by = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="posts",
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+
+    @property
+    def likes_count(self):
+        return self.likes.count()
+
+    @property
+    def comments_count(self):
+        return self.comments.filter(is_deleted=False).count()
+
+    class Meta:
+        ordering = ['-created_at']
+
+
+class Following(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="following"
+    )
+    follower = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="followers"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ['user', 'follower']
+
+
+class Like(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="likes"
+    )
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="likes"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ['user', 'post']
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="comments",
+    )
+    content = models.TextField()
+    created_by = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_deleted = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_at']
+    
