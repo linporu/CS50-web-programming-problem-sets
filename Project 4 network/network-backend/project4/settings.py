b9d73ea9b1580e2d24bdbcf9d23efb43b1157a45
linporu <qazwsx123456guy@gmail.com>
2024-12-24 22:01:17 +0800

    chore(be): enhance CORS and CSRF configuration
    - Added CorsMiddleware to the middleware settings for improved CORS support.
    - Consolidated CSRF_TRUSTED_ORIGINS into a single list for better management of trusted origins.

--- Project 4 network/network-backend/project4/settings.py
+++ Project 4 network/network-backend/project4/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -49,7 +50,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "project4.urls"
@@ -131,8 +131,7 @@ STATIC_URL = "/static/"
 
 CSRF_COOKIE_SAMESITE = "Lax"
 CSRF_COOKIE_SECURE = False
-CSRF_TRUSTED_ORIGINS = ["http://localhost:5173"]
-CSRF_TRUSTED_ORIGINS = ["http://localhost:5174"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost:5173", "http://localhost:5174"]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # Vite default port
